74f9a27b2c2745ab7b02d95e0b045de13353be46
kolawole-ysf <111007555+kolawole-ysf@users.noreply.github.com>
2022-10-31 08:27:52 +0000

    update

--- app.js
+++ app.js
@@ -14,22 +14,40 @@ function pageNavigation(){
         })
     }
     //sections display upon button click
+    // mainContent.addEventListener('click',(e)=>{
+    //     const id=e.target.dataset.id;
+    //           sections.forEach(section=>{
+    //         section.classList.remove('active');
+    //         if(id===section.id){
+    //             section.classList.add('active')   
+    //     }     
+    //     })
+    //    })
     mainContent.addEventListener('click',(e)=>{
         const id=e.target.dataset.id;
-        if(!id){
-            id='home';
+        if(id){
+            sections.forEach(section=>{
+                section.classList.remove('active');
+                 })
+                 const element=document.getElementById(id);
+                 element.classList.add('active');
         }
-        sections.forEach(section=>{
-            section.classList.remove('active');
-            if(id===section.id){
-                section.classList.add('active')   
-        }     
-        })
+             
        })
     
 }
 pageNavigation();
 
 //Contact form validation
+const myForm=document.querySelector('form');
+const nameInput=document.querySelector('.name').value;
+const subject=document.querySelector('.subject').value;
+const myMessage=document.querySelector('.message').value;
 
+myForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    if(!(isNaN(nameInput)) && nameInput.length<3){
+
+    }
+})
 
